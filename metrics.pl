cb1263d6bedd82df45f499bd4d707d801fbb145d
dr-riz <vizvan@gmail.com>
2014-05-20 22:33:46 -0400

    METRICS_STUB
    - metrics parses the json file
    - metrics is a json object
    - metrics.txt is the corresponding readable json file

--- metrics.pl
+++ metrics.pl
@@ -5,6 +5,7 @@ use Data::Dumper;
 use WWW::Mechanize;
 
 my $json_url = "http://mianemaster:50030/metrics?format=json";
+my $metrics_file = "C:/Users/mian/Documents/research/academic-profile/academic job/pdf/york/MRBurst/MRBurst/metrics";
 my $browser = WWW::Mechanize->new();
 
 # References
@@ -12,22 +13,22 @@ my $browser = WWW::Mechanize->new();
 
 # download the json page:
 # ref: http://beerpla.net/2008/03/27/parsing-json-in-perl-by-example-southparkstudioscom-south-park-episodes/
-print "Getting Job Tracker $json_url\n";
+print "Getting Job Tracker metrics json $json_url \n or reading $metrics_file...\n";
 $browser->get( $json_url );
 my $content = $browser->content();
 #my $json = $content; # from URL
 
-my $metrics_fn = "C:/Users/mian/Documents/research/academic-profile/academic job/pdf/york/MRBurst/metrics";
+my $metrics_fn = $metrics_file;
 open(my $fh, "<", $metrics_fn) or die "cannot open < $metrics_fn: $!";
 my $json = <$fh>; # from file
 
-print "json object: " . $json . "\n";
+#print "json object: " . $json . "\n";
 
 # parsing json: http://search.cpan.org/~bkb/JSON-Parse-0.30/lib/JSON/Parse.pod
 # http://www.tutorialspoint.com/json/json_perl_example.htm
 # method 1  most readable
 my $metrics = decode_json($json);
-print  Dumper($metrics);
+#print  Dumper($metrics);
 
 #job tracker level metrics -- global
 my $jobtracker = $metrics -> { 'mapred' } -> { 'jobtracker' }[0][1];
@@ -48,7 +49,7 @@ my $num_of_jobs = 0;
 if ((@jobs)) {
 	$num_of_jobs = @jobs;
   print "number of MR jobs (size of @jobs) = $num_of_jobs\n";
-  print Dumper(@jobs);
+  #print Dumper(@jobs);
 }
 
 if ($num_of_jobs > 0 ){
@@ -76,7 +77,7 @@ my $i=0, my $j=0, my $k=0;
 # following loop iterates and extracts from #jobs - array of array of hashes
 foreach my $tasks(@jobs){
 	$i++;
-	print "tasks = $tasks";
+	print "tasks = $tasks\n";
 	foreach my $task (@{$tasks}) {
     my $taskType = $metrics -> { 'fairscheduler' } -> { 'jobs' }[$j][0]-> {'taskType'}; # hash-level
    	$j++;
@@ -147,6 +148,5 @@ print "demand=" . $current_job{'demand'} . "\n";
 #  my $count = keys %$unit;
 #  print "number of elements in hash = " . $count . "\n";
 #}
-
 
 

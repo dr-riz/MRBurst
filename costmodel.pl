e6d36ff05c4cf9f36d476db328b9591f9881dad1
dr-riz <vizvan@gmail.com>
2014-05-21 15:42:17 -0400

    Coding up cost models

--- costmodel.pl
+++ costmodel.pl
@@ -10,7 +10,14 @@ my $core_master = "mianmaster";
 my $hdfs_jar_loc= "/user/root/remote/jar";
 
 my $json_url = "http://$edge_master:50030/metrics?format=json";
+my $metrics_file = "C:/Users/mian/Documents/research/academic-profile/academic job/pdf/york/MRBurst/MRBurst/metrics_existing_job";
 my $browser = WWW::Mechanize->new();
+
+my $mc = 0.122222222;
+my $mt = 52.13122222;
+my $t = 7.10880303;
+my $bw = 245; #285 Mbps
+
 
 # input: <binary.jar>, <class>, <local data dir>, <results data dir>
 my $numArgs = $#ARGV + 1;
@@ -20,55 +27,40 @@ my $data_dir = ();
 my $output_dir = ();
 my $output_to_edge = 0;
 
-if ($numArgs != 5) {
-	print "please, provide <binary_jar>, <class_name>, <data_dir>, <output_dir>, <output to edge 0|1>  as parameters\n";
-	exit(1);
+if (edgeBusy()) {
+	print "burst out to core\n";
 } else {
-  $binary_jar = $ARGV[0]; print "binary_jar: $binary_jar\n";
-  $class = $ARGV[1]; print "class: $class\n";
-  $data_dir = $ARGV[2]; print "input data dir: $data_dir\n";
-  $output_dir = $ARGV[3]; print "output data dir: $output_dir\n";
-  $output_to_edge = $ARGV[4]; print "write results on edge?: $output_to_edge\n";
-}
-
-#if (edgeBusy()) {
-if (1) {
-	print "edge busy, burst out to core\n";
-  remoteAccess();
-	#distcp();
-} else {
-	print "edge idle, submit in edge\n";
-  my $cmd = "hadoop jar $binary_jar $class $data_dir $output_dir";
-	print "executing command: $cmd\n";
-	my $retcode = system($cmd) == 0 or die "system failed with ret code?: $?";
-	print "retcode = $retcode\n";
+	print "submit in edge\n";
 }
 
 sub edgeBusy {
-# download the json page:
-# ref: http://beerpla.net/2008/03/27/parsing-json-in-perl-by-example-southparkstudioscom-south-park-episodes/
-	print "Getting Job Tracker $json_url\n";
+	# download the json page:
+	# ref: http://beerpla.net/2008/03/27/parsing-json-in-perl-by-example-southparkstudioscom-south-park-episodes/
+	print "Getting Job Tracker metrics json $json_url \n or reading $metrics_file...\n";
 	$browser->get( $json_url );
 	my $content = $browser->content();
 	my $json = $content; # from URL
 
-#	my $metrics_fn = "C:/Users/mian/Documents/tmp/metrics_kmeans";
-#	open(my $fh, "<", $metrics_fn) or die "cannot open < $metrics_fn: $!";
+  my $metrics_fn = $metrics_file;
+	#open(my $fh, "<", $metrics_fn) or die "cannot open < $metrics_fn: $!";
 	#my $json = <$fh>; # from file
 
-	print "json object: " . $json . "\n";
+	#print "json object: " . $json . "\n";
 
 # parsing json: http://search.cpan.org/~bkb/JSON-Parse-0.30/lib/JSON/Parse.pod
 # http://www.tutorialspoint.com/json/json_perl_example.htm
+# method 1  most readable
 	my $metrics = decode_json($json);
-#print  Dumper($metrics);
+	#print Dumper($metrics);
 
-#$uid =       $rHoH->{ $login }->{ 'uid' };   # method 1  most readable
+	# default of zero
+  my $maps_completed = 0;
+  my $pending_maps = 0;
 
 #job tracker level metrics -- global
 	my $jobtracker = $metrics -> { 'mapred' } -> { 'jobtracker' }[0][1];
 	print "jobtracker...\n";
-  print "*** job tracker level metrics -- global ***\n";
+	print "***  job tracker level metrics -- global ***\n";
 	#print  Dumper($jobtracker);
 	my $map_slots = $jobtracker -> {'map_slots'};
 	print "map_slots = $map_slots\n";
@@ -76,110 +68,96 @@ sub edgeBusy {
 	my $map_jobs_running = $jobtracker -> {'jobs_running'};
 	print "map_jobs_running = $map_jobs_running\n";
 
-#job level metrics -- local
-  print "*** job level metrics -- local ***\n";
- 	my @jobs = $metrics -> { 'fairscheduler' } -> { 'jobs' };
+	#job level metrics -- local
+	print "**    job level metrics -- local  *******\n";
+	my @jobs = $metrics -> { 'fairscheduler' } -> { 'jobs' };
 
-	my $num_of_jobs = 0;
-	if ((@jobs)) {
-		$num_of_jobs = @jobs;
-	}
-	print "number of MR jobs = $num_of_jobs\n";
+  if($map_jobs_running > 0) {
+		my $num_of_jobs = 0;
+		if ((@jobs)) {
+			$num_of_jobs = @jobs;
+  		print "number of MR jobs (size of @jobs) = $num_of_jobs\n";
+  		#print Dumper(@jobs);
+		}
 
-	if ($num_of_jobs > 0 ){
-		my $job = $metrics -> { 'fairscheduler' } -> { 'jobs' }[1][1]; #map
-  	my $runningMaps = $job -> {'runningTasks'};
-		print "runningMaps = $runningMaps\n";
+		my %current_job = ();
 
-  	my $mapDemand = $job -> {'demand'};
-		print "mapDemand = $mapDemand\n";
+		my $i=0, my $j=0, my $k=0, my $l=0;
+		# following loop iterates and extracts from #jobs - array of array of hashes
+		foreach my $tasks(@jobs){
+			$i++;
+			print "tasks = $tasks\n";
+			foreach my $task (@{$tasks}) {
+    		my $taskType = $metrics -> { 'fairscheduler' } -> { 'jobs' }[$j][0]-> {'taskType'}; # hash-level
+	   		$j++;
+  	  	if ($taskType eq "MAP" ) {
+    			print "map task...collect metrics\n";
+	    	} else {
+  	  		print "not a map task but:" . $taskType . "\n";
+    	  	next;
+    		}
+				#print "task = $task\n";
+    		my $map_task = 0; # boolean to represent map or reduce task
+	  		foreach my $unit (@{$task}) {  # unit is the atomic level hash or leaf node
+    			$k++;
+					#print "unit = $unit\n";
+
+					my $count = keys %$unit;
+  	    	if($count == 3) {
+	  	    	$current_job{'name'} = $unit->{'name'};
+      		}
+
+	      	if($count == 5) {
+  	    		$l++;
+    	  		$current_job{'runningTasks'} = $unit->{'runningTasks'};
+      	  	$current_job{'demand'} = $unit->{'demand'};
+
+	      	#print out contents of hash
+  		  	#while( my ($k, $v) = each %$unit ) {
+    			#  print "key: $k, value: $v.\n";
+    			#}
+	      	}
+		  	}
+			}
   	}
-	return $num_of_jobs;
-}
 
-#example command:
-#hadoop jar puma.jar org.apache.hadoop.examples.Classification hdfs://mianemaster/user/root/kmeans_30GB /user/root/kmeans_30GB_output
-
-sub distcp {
-
-# perl retcode: http://www.perlmonks.org/?node_id=486200
-# http://perldoc.perl.org/functions/system.html
-
-my $cmd =  "hadoop fs -rm -r -skipTrash $hdfs_jar_loc ";
-print "executing command: $cmd\n";
-system($cmd);# == 0 or die "system failed with ret code?: $?";
-
-$cmd = "hadoop fs -mkdir -p $hdfs_jar_loc";
-print "executing command: $cmd\n";
-my $retcode = system($cmd) == 0 or die "system failed with ret code?: $?";
-print "retcode = $retcode\n";
-#my $results = `$cmd`;
-#print $results;
-
-$cmd = "hadoop fs -put $binary_jar $hdfs_jar_loc";
-print "executing command: $cmd\n";
-$retcode = system($cmd) == 0 or die "system failed with ret code?: $?";
-print "retcode = $retcode\n";
-#$results = `$cmd`;
-#print $results;
-
-#copy jar file
-$cmd = "ssh $core_master 'hadoop distcp -overwrite hdfs://$edge_master:8020/$hdfs_jar_loc hdfs://$core_master:8020/$hdfs_jar_loc'";
-print "executing command: $cmd\n";
-$retcode = system($cmd) == 0 or die "system failed with ret code?: $?";
-print "retcode = $retcode\n";
-#$results = `$cmd`;
-#print $results;
-
-#copy data file  -- remove overwrite
-$cmd = "ssh $core_master 'hadoop distcp -overwrite hdfs://$edge_master:8020/$data_dir hdfs://$core_master:8020/$data_dir'";
-print "executing command: $cmd\n";
-$retcode = system($cmd) == 0 or die "system failed with ret code?: $?";
-print "retcode = $retcode\n";
-#$results = `$cmd`;
-#print $results;
-
-$cmd = "ssh $core_master 'hadoop fs -get $hdfs_jar_loc/$binary_jar .'";
-print "executing command: $cmd\n";
-$retcode = system($cmd); # == 0 or die "system failed with ret code?: $?"; uncomment die statement
-print "retcode = $retcode\n";
-#$results = `$cmd`;
-#print $results;
-
-# submit hadoop job in the remote hadoop
-$cmd = "ssh $core_master 'hadoop jar $binary_jar $class $data_dir $output_dir'";
-print "executing command: $cmd\n";
-$retcode = system($cmd) == 0 or die "system failed with ret code?: $?";
-print "retcode = $retcode\n";
-#$results = `$cmd`;
-#print $results;
-
-if ($output_to_edge) {
-	# copy the results back to the edge
-	$cmd = "hadoop distcp -overwrite hdfs://$core_master:8020/$output_dir hdfs://$edge_master:8020/$output_dir";
-	print "executing command: $cmd\n";
-  $retcode = system($cmd) == 0 or die "system failed with ret code?: $?";
-  print "retcode = $retcode\n";
-	#$results = `$cmd`;
-  #print $results;
-}
-}
-
-
-sub remoteAccess {
-#hadoop jar puma.jar org.apache.hadoop.examples.Grep hdfs://mianemaster/user/root/wikipedia_50GB /user/root//user/root/wikipedia_output/Grep_ra Peace 2>&1 | tee log.log
-
-# submit hadoop job in the remote hadoop
-	if ($output_to_edge) {
-		# write the results back to the edge
- 		my $cmd = "ssh $core_master 'hadoop jar $binary_jar $class hdfs://$edge_master/$data_dir hdfs://$edge_master/$output_dir'";
+		my $cmd = "hadoop job -counter " . $current_job{'name'} . " org.apache.hadoop.mapreduce.JobCounter TOTAL_LAUNCHED_MAPS";
 		print "executing command: $cmd\n";
-		my $retcode = system($cmd) == 0 or die "system failed with ret code?: $?";
-		print "retcode = $retcode\n";
-	} else {
-		my $cmd = "ssh $core_master 'hadoop jar $binary_jar $class hdfs://$edge_master/$data_dir $output_dir'";
-		print "executing command: $cmd\n";
-		my $retcode = system($cmd) == 0 or die "system failed with ret code?: $?";
-		print "retcode = $retcode\n";
+		my $output = `$cmd`;
+		print "output: $output\n";
+
+		if ($output =~ /([0-9]+)/) {
+  		$maps_completed = $1;
+		} else {
+    	$maps_completed = 0;
+    }
+
+		$pending_maps = $current_job{'demand'};
+    $maps_completed = $maps_completed - $current_job{'runningTasks'};
+
+		print "maps_completed =" . $maps_completed . "\n";
+		print "i=$i, j=$j, k=$k, l=$l\n";
+		print "map profile for a job...\n";
+		print "[job] name=" . $current_job{'name'} . "\n";
+		print "runningTasks [maps]=" . $current_job{'runningTasks'} . "\n";
+		print "pending_maps or demand=" . $pending_maps . "\n";
 	}
+
+  my $slots = $map_slots;
+  my $data = 50;
+
+  my $cost_e = ($data - ($maps_completed * $mc))/$slots * $t;
+  my $cost_i = ($data * $t)/$slots;
+  my $cost_l = $cost_e + $cost_i;
+
+  # We begin by assuming the core is idle.
+  my $cost_ri = ($data * $t) / $slots + ($data * 1024) / ($bw * 60/8);
+
+  print "execution cost of existing job = $cost_e\n";
+  print "execution cost new job = $cost_i\n";
+  print "cost of executing new job locally (edge) in presence of old job = $cost_l\n";
+  print "cost of executing new job remotely (core) = $cost_ri\n";
+
+  return ($cost_l > $cost_ri );
 }
+

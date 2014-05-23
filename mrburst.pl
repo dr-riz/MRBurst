058cf411cfccd34e91688e6f8c88f766ed8a8370
dr-riz <vizvan@gmail.com>
2014-05-23 10:57:06 -0400

    Merger_mrburst_costmodel

--- mrburst.pl
+++ mrburst.pl
@@ -12,6 +12,24 @@ my $hdfs_jar_loc= "/user/root/remote/jar";
 my $json_url = "http://$edge_master:50030/metrics?format=json";
 my $browser = WWW::Mechanize->new();
 
+my $bw = 245; #285 Mbps
+my %job_profile = ();
+$job_profile{'ii'}{'mc'} =  0.122222222; #inverted index
+$job_profile{'ii'}{'mt'} =  52.13122222;
+$job_profile{'ii'}{'t'} =  7.10880303;
+
+$job_profile{'wc'}{'mc'} =  0.122222222; #word count
+$job_profile{'wc'}{'mt'} =  50.519;
+$job_profile{'wc'}{'t'} =  6.888954545;
+
+$job_profile{'grep'}{'mc'} =  0.122222222; #grep
+$job_profile{'grep'}{'mt'} =  8.960333333;
+$job_profile{'grep'}{'t'} =  1.221863636;
+
+my $mc = 0.122222222;
+my $mt = 52.13122222;
+my $t = 7.10880303;
+
 # input: <binary.jar>, <class>, <local data dir>, <results data dir>
 my $numArgs = $#ARGV + 1;
 my $binary_jar = ();
@@ -20,6 +38,7 @@ my $data_dir = ();
 my $output_dir = ();
 my $output_to_edge = 0;
 my $new_job_name = ();
+my $existing_job = 0; # by default, there is no existing job
 my $existing_job_data_size = 0;
 
 if ($numArgs != 5) {
@@ -35,48 +54,51 @@ if ($numArgs != 5) {
 
   print "class: $class, new_job_name: $new_job_name\n";
 
+  # extract existing job metrics
   my $existing_job_name = (); my $existing_job_id = ();
   my $cmd = 'mapred job -list | egrep \'^job\' | awk \'{print $1}\' | xargs -n 1 -I {} sh -c "mapred job -status {} | egrep \'^tracking\' | awk \'{print \$3}\'" | xargs -n 1 -I{} sh -c "echo -n {} | sed \'s/.*jobid=//\'; echo -n \' \';curl -s -XGET {} | grep \'Job Name\' | sed \'s/.* //\' | sed \'s/<br>//\'"';
   my $existing_job_name_str = `$cmd`;
   if ($existing_job_name_str =~ /([_a-zA-Z0-9]+) ([a-zA-Z]+)$/) {
     $existing_job_id = $1;
     $existing_job_name = $2;
-	}
-  print "existing_job_id = $existing_job_id\n";
-	print "existing_job_name = $existing_job_name\n";
+    $existing_job = 1;
 
-  $cmd = "mapred job -status $existing_job_id | grep file";
-  my $output = `$cmd`;
-  my $job_staging_file = ();
-	if ($output =~  /([:_0-9\/\.a-zA-Z]+)$/) {
-    $job_staging_file = $1;
-	}
-  print "job_staging_file:$job_staging_file\n";
+    print "existing_job_id = $existing_job_id\n";
+		print "existing_job_name = $existing_job_name\n";
 
-  $cmd = "hadoop fs -cat $job_staging_file | grep mapred.input.dir";
-  $output = `$cmd`;
-  my $existing_job_input_folder = ();
-  if ($output =~ /<value>([:_0-9\/\.a-zA-Z]+)<\/value>/) {
-		$existing_job_input_folder = $1;
-	}
-  print "existing_job_input_folder:$existing_job_input_folder\n";
+	  $cmd = "mapred job -status $existing_job_id | grep file";
+  	my $output = `$cmd`;
+	  my $job_staging_file = ();
+		if ($output =~  /([:_0-9\/\.a-zA-Z]+)$/) {
+    	$job_staging_file = $1;
+		}
+  	print "job_staging_file:$job_staging_file\n";
 
-  $cmd = "hadoop fs -du -s $existing_job_input_folder";
-  print "cmd=$cmd\n";
-  $output = `$cmd`;
-  print "output=$output\n";
+	  $cmd = "hadoop fs -cat $job_staging_file | grep mapred.input.dir";
+  	$output = `$cmd`;
+	  my $existing_job_input_folder = ();
+  	if ($output =~ /<value>([:_0-9\/\.a-zA-Z]+)<\/value>/) {
+			$existing_job_input_folder = $1;
+		}
+  	print "existing_job_input_folder:$existing_job_input_folder\n";
 
-	if ($output =~ /^([0-9]+)/) {
-  	$existing_job_data_size = $1;
-	}
+	  $cmd = "hadoop fs -du -s $existing_job_input_folder";
+  	#print "cmd=$cmd\n";
+	  $output = `$cmd`;
+  	#print "output=$output\n";
 
-  $existing_job_data_size  = $existing_job_data_size /(1024 * 1024 * 1024); #in GB
-  print "existing_job_data_size = $existing_job_data_size GB\n";
+		if ($output =~ /^([0-9]+)/) {
+  		$existing_job_data_size = $1;
+		}
+
+  	$existing_job_data_size  = $existing_job_data_size /(1024 * 1024 * 1024); #in GB
+	  print "existing_job_data_size = $existing_job_data_size GB\n";
+  }
 
   $data_dir = $ARGV[2]; print "input data dir: $data_dir\n";
   my $input_data_size = 0;
   $cmd = "hadoop fs -du -s $data_dir";
-  $output = `$cmd`;
+  my $output = `$cmd`;
 	if ($output =~ /^([0-9]+)/) {
   	$input_data_size = $1;
 	}
@@ -88,17 +110,17 @@ if ($numArgs != 5) {
   $output_to_edge = $ARGV[4]; print "write results on edge?: $output_to_edge\n";
 }
 
-#if (edgeBusy()) {
-if (1) {
+if (edgeBusy()) {
+#if (1) {
 	print "edge busy, burst out to core\n";
   #remoteAccess();
 	#distcp();
 } else {
 	print "edge idle, submit in edge\n";
-  my $cmd = "hadoop jar $binary_jar $class $data_dir $output_dir";
-	print "executing command: $cmd\n";
-	my $retcode = system($cmd) == 0 or die "system failed with ret code?: $?";
-	print "retcode = $retcode\n";
+  #my $cmd = "hadoop jar $binary_jar $class $data_dir $output_dir";
+#	print "executing command: $cmd\n";
+#	my $retcode = system($cmd) == 0 or die "system failed with ret code?: $?";
+#	print "retcode = $retcode\n";
 }
 
 sub edgeBusy {
@@ -113,14 +135,17 @@ sub edgeBusy {
 #	open(my $fh, "<", $metrics_fn) or die "cannot open < $metrics_fn: $!";
 	#my $json = <$fh>; # from file
 
-	print "json object: " . $json . "\n";
+	#print "json object: " . $json . "\n";
 
 # parsing json: http://search.cpan.org/~bkb/JSON-Parse-0.30/lib/JSON/Parse.pod
 # http://www.tutorialspoint.com/json/json_perl_example.htm
+# method 1  most readable
 	my $metrics = decode_json($json);
 #print  Dumper($metrics);
 
-#$uid =       $rHoH->{ $login }->{ 'uid' };   # method 1  most readable
+	# default of zero
+  my $maps_completed = 0;
+  my $pending_maps = 0;
 
 #job tracker level metrics -- global
 	my $jobtracker = $metrics -> { 'mapred' } -> { 'jobtracker' }[0][1];
@@ -133,25 +158,97 @@ sub edgeBusy {
 	my $map_jobs_running = $jobtracker -> {'jobs_running'};
 	print "map_jobs_running = $map_jobs_running\n";
 
-#job level metrics -- local
-  print "*** job level metrics -- local ***\n";
- 	my @jobs = $metrics -> { 'fairscheduler' } -> { 'jobs' };
+	#job level metrics -- local
+	print "**    job level metrics -- local  *******\n";
+	my @jobs = $metrics -> { 'fairscheduler' } -> { 'jobs' };
 
-	my $num_of_jobs = 0;
-	if ((@jobs)) {
-		$num_of_jobs = @jobs;
-	}
-	print "number of MR jobs = $num_of_jobs\n";
+  if($map_jobs_running > 0) {
+		my $num_of_jobs = 0;
+		if ((@jobs)) {
+			$num_of_jobs = @jobs;
+  		print "number of MR jobs (size of @jobs) = $num_of_jobs\n";
+  		#print Dumper(@jobs);
+		}
 
-	if ($num_of_jobs > 0 ){
-		my $job = $metrics -> { 'fairscheduler' } -> { 'jobs' }[1][1]; #map
-  	my $runningMaps = $job -> {'runningTasks'};
-		print "runningMaps = $runningMaps\n";
+		my %current_job = ();
 
-  	my $mapDemand = $job -> {'demand'};
-		print "mapDemand = $mapDemand\n";
+		my $i=0, my $j=0, my $k=0, my $l=0;
+		# following loop iterates and extracts from #jobs - array of array of hashes
+		foreach my $tasks(@jobs){
+			$i++;
+			print "tasks = $tasks\n";
+			foreach my $task (@{$tasks}) {
+    		my $taskType = $metrics -> { 'fairscheduler' } -> { 'jobs' }[$j][0]-> {'taskType'}; # hash-level
+	   		$j++;
+  	  	if ($taskType eq "MAP" ) {
+    			print "map task...collect metrics\n";
+	    	} else {
+  	  		print "not a map task but:" . $taskType . "\n";
+    	  	next;
+    		}
+				#print "task = $task\n";
+    		my $map_task = 0; # boolean to represent map or reduce task
+	  		foreach my $unit (@{$task}) {  # unit is the atomic level hash or leaf node
+    			$k++;
+					#print "unit = $unit\n";
+
+					my $count = keys %$unit;
+  	    	if($count == 3) {
+	  	    	$current_job{'name'} = $unit->{'name'};
+      		}
+
+	      	if($count == 5) {
+  	    		$l++;
+    	  		$current_job{'runningTasks'} = $unit->{'runningTasks'};
+      	  	$current_job{'demand'} = $unit->{'demand'};
+
+	      	#print out contents of hash
+  		  	#while( my ($k, $v) = each %$unit ) {
+    			#  print "key: $k, value: $v.\n";
+    			#}
+	      	}
+		  	}
+			}
   	}
-	return $num_of_jobs;
+
+		my $cmd = "hadoop job -counter " . $current_job{'name'} . " org.apache.hadoop.mapreduce.JobCounter TOTAL_LAUNCHED_MAPS";
+		print "executing command: $cmd\n";
+		my $output = `$cmd`;
+		print "output: $output\n";
+
+		if ($output =~ /([0-9]+)/) {
+  		$maps_completed = $1;
+		} else {
+    	$maps_completed = 0;
+    }
+
+		$pending_maps = $current_job{'demand'};
+    $maps_completed = $maps_completed - $current_job{'runningTasks'};
+
+		print "maps_completed =" . $maps_completed . "\n";
+		print "i=$i, j=$j, k=$k, l=$l\n";
+		print "map profile for a job...\n";
+		print "[job] name=" . $current_job{'name'} . "\n";
+		print "runningTasks [maps]=" . $current_job{'runningTasks'} . "\n";
+		print "pending_maps or demand=" . $pending_maps . "\n";
+	}
+
+  my $slots = $map_slots;
+  my $data = 50;
+
+  my $cost_e = ($data - ($maps_completed * $job_profile{'ii'}{'mc'}))/$slots * $job_profile{'ii'}{'t'};
+  my $cost_i = ($data * $job_profile{'grep'}{'t'})/$slots;
+  my $cost_l = $cost_e + $cost_i;
+
+  # We begin by assuming the core is idle.
+  my $cost_ri = ($data * $job_profile{'grep'}{'t'}) / $slots + ($data * 1024) / ($bw * 60/8);
+
+  print "execution cost of existing job = $cost_e\n";
+  print "execution cost new job = $cost_i\n";
+  print "cost of executing new job locally (edge) in presence of old job = $cost_l\n";
+  print "cost of executing new job remotely (core) = $cost_ri\n";
+
+  return ($cost_l > $cost_ri );
 }
 
 #example command:
